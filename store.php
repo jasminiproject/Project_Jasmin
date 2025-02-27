58d9258d657600b0054e31299b66852407a87820
jasminiproject <96034313+jasminiproject@users.noreply.github.com>
2025-02-27 20:39:57 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,18 +1,17 @@
 <?php
 error_reporting(0);
-// Connect to MySQL (XAMPP)
+
 $conn = new mysqli("localhost", "root", "", "college_info");
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get user input
+
 $name = $_POST['name'];
 $age = $_POST['age'];
 
-// Insert into database
+
 $sql = "INSERT INTO student (name, age) VALUES ('$name', '$age')";
 if ($conn->query($sql) === TRUE) {
     echo "User data stored successfully!";
@@ -20,6 +19,6 @@ if ($conn->query($sql) === TRUE) {
     echo "Error: " . $conn->error;
 }
 
-// Close connection
+
 $conn->close();
 ?>
